2269edfc1ad4853ef91b970ebb14fe63a678ed07
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-04-01 14:36:53 +0700

    Memasukan function main (struct3)

--- Struct1/Struct3.cpp
+++ Struct1/Struct3.cpp
@@ -6,9 +6,34 @@ struct DetailAlamat {
 	string kota;
 };
 
-struct mahasiswa {
+struct Mahasiswa {
 	string nim;
 	string nama;
 	string alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Induk Mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat;
+
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : ";
+	cout << "\n\t Desa : " << mhs.alamat.desa;
+	cout << "\n\t Kota : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
