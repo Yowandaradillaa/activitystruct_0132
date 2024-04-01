f6127abe78d0fe68373bb64e257cdb0772e8f190
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-04-01 14:18:20 +0700

    Menampilkan dan memanggil variabel

--- Struct1/Struct2.cpp
+++ Struct1/Struct2.cpp
@@ -14,5 +14,25 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs;
+	cout << "Nomor Induk Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
 
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat;
+
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : ";
+	cout << "\n\t Desa : " << mhs.alamat;
+	cout << "\n\t Kota : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
 }
